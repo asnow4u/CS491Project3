ba66e2e4f254e60e3b3cbbc74a35ebd2680a1103
asnow4u <asnow4u@gmail.com>
2019-10-16 10:09:50 -0700

    Started Project

---
+++ forward.cpp
@@ -0,0 +1,10 @@
+void Forward( float th1, float th2, float th3,  float l1, float l2,
+	 glm::mat4& m1g, glm::mat4& m2g, glm::mat4& m3g )
+{
+}
+
+void WhoAmI( std::string &yourName, std::string &yourEmailAddress )
+{
+        yourName = "Andrew Snow";
+        yourEmailAddress = "snowan@oregonstate.edu" ;
+}
